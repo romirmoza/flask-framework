1f5a959b0a82572bfe13d2369ba8d48b6a39aebb
Romir Moza <romirmoza@Romirs-MacBook-Air.local>
2019-09-16 03:05:42 -0400

    Added bokeh figures

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
-from flask import Flask, render_template, request, redirect
+import quandl
+import pandas as pd
+from bokeh.plotting import figure, output_file, show
+from bokeh.embed import components
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+def drawplot(col, tick):
+    quandl.ApiConfig.api_key = 'j2d-o1VPfbxdbifUEsLj'
+    data = quandl.get_table('WIKI/PRICES', ticker = tick,
+                qopts = { 'columns': ['ticker', 'date', col] },
+                date = { 'gte': '2016-12-31', 'lte': '2017-12-31' },
+                paginate=True)
+    x = data['date']
+    y = data[col]
+    p = figure(plot_width=1200, plot_height=375, x_axis_type="datetime")
+    p.line(x,y)
+    return p
+
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-  return render_template('index.html')
+    if request.method == 'POST':
+        tick = request.form['ticker']
+        col = request.form['options']
+        p = drawplot(col,tick)
+        script, div = components(p)
+        return render_template('graph.html', script=script, div=div)
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
+
+@app.route('/graph')
+def graph():
+    return render_template('graph.html')
+
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507)
